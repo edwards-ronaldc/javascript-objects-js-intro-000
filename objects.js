dbc035b22c512d792a6f813e6562778580c6c55e
Ronald C Edwards <rce246@stern.nyu.edu>
2020-01-17 19:57:55 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,2 @@
 var playlist = {"Katy Perry":"Teenage Dream", "Mabel":"Mad Love", "Kanye West":"Love Lockdown", "Whitney Houston":"Greatest Love of all" }
+function updatePlaylist(playlist, artistName, songTitle) {playlist[artistName] = songTitle; return playlist}
